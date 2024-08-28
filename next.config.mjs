82f95c4eb9a5fdbe67e8a8caa4c8260aaf134540
Rabeya akter <rabeyaakterbdcit@gmail.com>
2024-08-28 11:02:35 +0600

    added facebook login

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,29 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    images: {
+      remotePatterns: [
+        {
+          protocol: 'https',
+          hostname: 'lh3.googleusercontent.com',
+          port: '',
+          pathname: '/**',
+        },
+        {
+          protocol: 'https',
+          hostname: 'avatars.githubusercontent.com',
+          port: '',
+          pathname: '/**',
+        },
+        {
+          protocol: 'https',
+          hostname: 'scontent.fdac166-1.fna.fbcdn.net',
+          port: '',
+          pathname: '/**',
+        },
+      ],
+    },
+    
+  };
+  
+  export default nextConfig;
+  
